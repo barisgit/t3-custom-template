bd758f39c22fb5b818ea5791e5dbabbf0aae5fff
barisgit <baristovnik@gmail.com>
2024-10-03 13:23:50 +0200

    Internationalization fixes and protected routes

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
+import { useTranslations } from "next-intl";
 
 export default function Error({
   error,
@@ -9,22 +10,25 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const t = useTranslations("error");
+
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="-mt-24 flex min-h-screen flex-col items-center justify-center">
+      <div className="text-center">
+        <h2 className="mb-4 text-2xl font-bold">{t("title")}</h2>
+        <p className="mb-4">{t("message")}</p>
+        <button
+          onClick={() => reset()}
+          className="text-blue-500 hover:underline"
+        >
+          {t("tryAgain")}
+        </button>
+      </div>
     </div>
   );
 }
