73cd21d7b07f3532a2b02c2f2c4fdc2249f16e2a
barisgit <baristovnik@gmail.com>
2024-10-03 18:47:39 +0200

    Add dropdown

--- src/app/_components/SettingsDropdown.tsx
+++ src/app/_components/SettingsDropdown.tsx
@@ -1,16 +1,57 @@
+"use client";
+
 import React from "react";
+import { useTranslations } from "next-intl";
+import { Settings, Globe } from "lucide-react";
+import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSubTrigger,
+  DropdownMenuPortal,
+  DropdownMenuSubContent,
+  DropdownMenuSub,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import ThemeSwitch from "./ThemeSwitch";
+import LocaleSwitcher from "./LocaleSwitcher";
+import { useLocale } from "next-intl";
 
-const SettingsDropdown: React.FC = () => {
+export default function SettingsDropdown() {
+  const t = useTranslations("SettingsDropdown");
+  const locale = useLocale();
+
   return (
-    <div className="dropdown">
-      <button className="btn">Settings</button>
-      <div className="dropdown-content">
-        <a href="#">Profile</a>
-        <a href="#">Account</a>
-        <a href="#">Logout</a>
-      </div>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" aria-label={t("settings")}>
+          <Settings className="h-[1.2rem] w-[1.2rem]" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>{t("settings")}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <ThemeSwitch />
+          </DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <Globe className="mr-2 h-4 w-4" />
+              <span>{t("language")}</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <LocaleSwitcher />
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
-};
-
-export default SettingsDropdown;
+}
