2fbfadc27545b1e904d4f7b5a6dc7798774f990d
barisgit <baristovnik@gmail.com>
2024-10-03 15:22:56 +0200

    Protected routes

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -4,14 +4,37 @@ import { type NextRequest } from "next/server";
 import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
-
+import { getAuth } from "@clerk/nextjs/server";
+import { db } from "~/server/db";
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
  */
+
+// Define the User type based on your database schema
+import { type User } from "~/types/global";
+
 const createContext = async (req: NextRequest) => {
+  const { userId } = getAuth(req);
+
+  let user: User | null = null;
+  if (userId) {
+    user = await db.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        role: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        // Add other fields you need
+      },
+    });
+  }
+
   return createTRPCContext({
     headers: req.headers,
+    user: user,
   });
 };
 
@@ -25,7 +48,7 @@ const handler = (req: NextRequest) =>
       env.NODE_ENV === "development"
         ? ({ path, error }) => {
             console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
             );
           }
         : undefined,
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,1 +1,10 @@
 export type Messages = Record<string, string>; // Adjust based on your JSON structure
+
+export type User = {
+  id: string;
+  role: "USER" | "ADMIN" | "SUPER_ADMIN"; // Adjust these roles as needed
+  email: string;
+  firstName: string | null;
+  lastName: string | null;
+  // Add other fields you need, but be cautious about including sensitive information
+};
